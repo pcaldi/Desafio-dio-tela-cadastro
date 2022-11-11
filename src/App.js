449e6db25536fc0c839329093a5616b910b6e3bd
Paulo Caldi <pcaldi@gmail.com>
2022-11-11 18:38:24 -0300

    tela de login

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-
 } from "react-router-dom";
 
-import { Home } from "./pages/home";
-import { Login } from "./pages/login";
+import { Feed } from "./pages/Feed";
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Cadastro } from './pages/Cadastro'
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <Router>
-      <Routes> 
-
-      <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-    
-    </Routes>
+    <Router basename={process.env.PUBLIC_URL}>
+     <GlobalStyle />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+     </Routes >
     </Router>
   );
 }
 
 export default App;
+
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,59 @@
-import { Link } from "react-router-dom"
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import {Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from "./styles";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+
+const schema = yup.object({
+    name: yup.string().email('email não é válido').required('Campo obrigatório'),
+    password : yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
+  }).required();
 
 
 const Login = () => {
+
+
+  const { handleSubmit, control, formState: { errors, isValid } } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
+
+  console.log(isValid, errors);
+
+
+  const onSubmit = data => console.log(data);
+
     return (<>
-        <h1>Login</h1>
-        <Link to ="/"> Voltar para Home </Link>
+        <Header/>
+        <Container>
+            <Column>
+                <Title>
+                    A plataforma para você aprender com experts dominar as principais tecnologias
+                    e entrar mais rápido nas empresas mais desejadas.
+                </Title> 
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input name="email"   control={control} placeholder= "E-mail" />
+                    <Input name="password"  control={control} placeholder= "Senha" type="password"/>
+                    <Button title="Entrar" variant="secondary" type="submit"/>
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <CriarText>Criar conta</CriarText>
+                    </Row>
+                </Wrapper>          
+            </Column>
+        </Container>
+   
 
     </>)
 
